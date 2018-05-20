98ec3e23b2e59f68fd3c29b4d81e9ef25034ca85
Gratteur <38252816+Gratteur@users.noreply.github.com>
2018-05-20 10:41:13 +0200

    Contenu brut

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ def gintoki():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=1001, host='0.0.0.0',)
+    app.run(debug=True, port=1001, host='0.0.0.0', threaded=True)
 
 
 
